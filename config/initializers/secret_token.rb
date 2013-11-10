d84f0ef09f6fbf0d05778ee392ecdec77cd33176
Colby Aley <colby@aley.me>
2013-11-09 19:33:50 -0800

    Adding rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coffeebean::Application.config.secret_token = '0105ccf3149e56d1d1b16675214b6ae9d00936e5a41e4cc779b3399c8faffe84d9f0bfb120227757136139575b5de2dfd803193e2864646615d7ab2fadb09be3'
